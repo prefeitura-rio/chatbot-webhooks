d2ee37da293d0e716f01207cb4467da612b68d5e
Bruno <brunodepauloalmeida@gmail.com>
2023-12-03 23:11:25 +0000

    fix: typo problems

--- chatbot_webhooks/webhooks/tags.py
+++ chatbot_webhooks/webhooks/tags.py
@@ -1556,8 +1556,8 @@ async def da_consulta_debitos_contribuinte(request_data: dict) -> tuple[str, dic
     if parameters["da1_tipo_de_consulta"] in [
         "Inscrição Imobiliária",
         "Certidão da Dívida Ativa",
+        "CPF/CNPJ",
         "Execução Fiscal",
-        "Auto de Infração",
     ]:
         msg += f'{mapeia_descricoes[parameters["da1_tipo_de_consulta"]]}: {parameters[mapeia_variaveis[parameters["da1_tipo_de_consulta"]]]}'
     else:
@@ -1609,10 +1609,26 @@ async def da_consulta_debitos_contribuinte(request_data: dict) -> tuple[str, dic
     parameters["dicionario_itens"] = itens_pagamento
     parameters["total_itens_pagamento"] = indice
     parameters["mensagem_divida_contribuinte"] = msg
-    parameters["guias_quantidade_total"] = len(parameters.get("lista_guias", []))
-    parameters["efs_cdas_quantidade_total"] = len(parameters.get("lista_efs", [])) + len(
-        parameters.get("lista_cdas", [])
-    )
+    parameters["lista_efs"] = parameters.get("lista_efs", []) if parameters.get("lista_efs", None) is not None else []
+    parameters["lista_cdas"] = parameters.get("lista_cdas", []) if parameters.get("lista_cdas", None) is not None else []
+    parameters["lista_guias"] = parameters.get("lista_guias", []) if parameters.get("lista_guias", None) is not None else []
+    
+    try:
+        parameters["guias_quantidade_total"] = len(parameters.get("lista_guias", []))
+    except:
+        parameters["guias_quantidade_total"] = 0
+
+    try:
+        qtd_efs = len(parameters.get("lista_efs", []))
+    
+    except:
+        qtd_efs = 0
+    try:
+        qtde_cdas = len(parameters.get("lista_cdas", []))
+    except:
+        qtde_cdas = 0
+    
+    parameters["efs_cdas_quantidade_total"] = qtd_efs + qtde_cdas
 
     # Definindo parâmetros salto_total parcelado e não parcelado
     parameters["total_nao_parcelado"] = len(
